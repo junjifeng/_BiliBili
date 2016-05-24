31ced794a94e1f0f6680bfffdcbe26e5664fb584
耿杰 <misaka14@outlook.com>
2016-05-24 13:30:52 +0800

    Initial Project

---
+++ WTBilibili/Home-首页/Live-直播/Controller/WTLiveViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WTLiveViewController.swift
+//  WTBilibili
+//
+//  Created by 无头骑士 GJ on 16/5/3.
+//  Copyright © 2016年 无头骑士 GJ. All rights reserved.
+//
+
+import UIKit
+
+class WTLiveViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.greenColor()
+    }
+}
